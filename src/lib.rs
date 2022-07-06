d096e7099f3a5dd3f41483edea967bfb125bf17c
Daniel Kessl <dan@kessl.net>
2022-07-06 15:36:54 +0200

    Use console error panic hook

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
+extern crate console_error_panic_hook;
+
 use wasm_bindgen::prelude::*;
+use std::panic;
 
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
     Ok(())
 }
 
